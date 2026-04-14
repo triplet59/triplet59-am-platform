86e58cc46bb743e82268d35b2c1ea9640e70c260
Derry Thornalley <derrythornalley@Derrys-Mac-Studio.local>
2026-04-14 09:44:40 +0400

    Add top-level stop truth test

--- scripts/dashboard_streamlit.py
+++ scripts/dashboard_streamlit.py
@@ -9,6 +9,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
+st.stop()
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.platypus import Paragraph, SimpleDocTemplate
 from scripts.data_loader import INDEX_POOL, load_benchmark, load_index
